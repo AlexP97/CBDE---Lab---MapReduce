c391bfc1ea654d4b28f170e11d4e26b43e768652
Alejandro Pérez Ortiz <alejandro.perez.ortiz@est.fib.upc.edu>
2018-10-23 14:14:12 +0200

    Cambio en el inicio de clave

--- 04-HBase-JavaProject/src/main/java/cbde/labs/hbase_mapreduce/writer/MyHBaseWriter_KeyDesign.java
+++ 04-HBase-JavaProject/src/main/java/cbde/labs/hbase_mapreduce/writer/MyHBaseWriter_KeyDesign.java
@@ -1,15 +1,16 @@
 package cbde.labs.hbase_mapreduce.writer;
 
 public class MyHBaseWriter_KeyDesign extends MyHBaseWriter {
-	int otherKey = 0;
+	int otherKey = 1;
 
 	protected String nextKey() {
 		String new_key;
 		if(this.data.get("type") == "type_3" && this.data.get("region") == "0")
 			new_key = String.valueOf(this.key);
-		else
+		else {
 			new_key = "a"+String.valueOf(this.otherKey);
-		otherKey++;
+			otherKey++;
+		}
 		return new_key;
 	}
 		

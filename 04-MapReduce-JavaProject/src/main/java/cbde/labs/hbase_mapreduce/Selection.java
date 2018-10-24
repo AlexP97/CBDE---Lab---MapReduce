68a4dc8dae797defc8428a2873dea428dfc27115
Alejandro Pérez Ortiz <alejandro.perez.ortiz@est.fib.upc.edu>
2018-10-24 11:38:55 +0200

    Select funcional

--- 04-MapReduce-JavaProject/src/main/java/cbde/labs/hbase_mapreduce/Selection.java
+++ 04-MapReduce-JavaProject/src/main/java/cbde/labs/hbase_mapreduce/Selection.java
@@ -1,15 +1,54 @@
 package cbde.labs.hbase_mapreduce;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Reducer.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import cbde.labs.hbase_mapreduce.CartesianProduct.CartesianMapper;
+import cbde.labs.hbase_mapreduce.CartesianProduct.CartesianReducer;
 
 public class Selection extends JobMapReduce {
     
+	//mirar el projection y será igual cogiendo type 3 y clave usar 1,2,3,....
+	
+	
 	public Selection() {
 		this.input = null;
 		this.output = null;
+	}
+	
+	public static class SelectionMapper extends Mapper<Text, Text, IntWritable, Text> {
+		
+		private static int N = 100;
+		
+		private int newKey = 0;
+		
+		public void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+			// Obtain the parameters sent during the configuration of the job
+			String where = context.getConfiguration().getStrings("where")[0];
+			String valor = context.getConfiguration().getStrings("valor")[0];
+			// Since the value is a CSV, just get the lines split by commas
+			String[] arrayValues = value.toString().split(",");
+			String whereValue = Utils.getAttribute(arrayValues, where);
+			// Do the cartesian product and emit it
+			if (whereValue.equals(valor)) {
+				context.write(new IntWritable(this.newKey), value);
+				newKey++;
+			}
+		}
+		
 	}
 	
 	public boolean run() throws IOException, ClassNotFoundException, InterruptedException {
@@ -24,7 +63,18 @@ public class Selection extends JobMapReduce {
     public static void configureJob(Job job, String pathIn, String pathOut) throws IOException, ClassNotFoundException, InterruptedException {
         job.setJarByClass(Selection.class);
 
-        // Configure the rest of parameters required for this job
-        // Take a look at the provided examples: Projection, AggregationSum and CartesianProduct
+        job.setMapperClass(SelectionMapper.class);
+        job.setMapOutputKeyClass(IntWritable.class);
+        job.setMapOutputValueClass(Text.class);
+        
+        
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(Text.class);
+        
+        job.setInputFormatClass(SequenceFileInputFormat.class);
+        FileInputFormat.addInputPath(job, new Path(pathIn));
+        FileOutputFormat.setOutputPath(job, new Path(pathOut));
+        job.getConfiguration().setStrings("where", "type");
+        job.getConfiguration().setStrings("valor", "type_1"); 
     }
 }
